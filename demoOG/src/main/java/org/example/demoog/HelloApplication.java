43622f493e7850e311fae10bc0db56ff4c450272
danielle bemfica <danielleludovicoo@gmail.com>
2025-04-11 15:00:11 +0200

    commit

--- demoOG/src/main/java/org/example/demoog/HelloApplication.java
+++ demoOG/src/main/java/org/example/demoog/HelloApplication.java
@@ -1,4 +1,4 @@
-package org.example.demo;
+package org.example.demoog;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- demoOG/src/main/java/org/example/demoog/HelloController.java
+++ demoOG/src/main/java/org/example/demoog/HelloController.java
@@ -1,11 +1,21 @@
-package org.example.demo;
+package org.example.demoog;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    static int contador;
     @FXML
     private Label welcomeText;
+
+    @FXML
+    public Button contar;
+
+    @FXML
+    public void initialize(){
+        contar.setOnAction(e -> System.out.println(++contador));
+    }
 
     @FXML
     protected void onHelloButtonClick() {
